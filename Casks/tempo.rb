36f182f0ead2303e6f60fb2483b7bb8345e88eec
caereforge <277028088+caereforge@users.noreply.github.com>
2026-05-12 20:32:29 +0200

    auto: modifica tempo.rb

--- Casks/tempo.rb
+++ Casks/tempo.rb
@@ -1,8 +1,6 @@
 cask "tempo" do
-  disable! date: "2026-05-11", because: "is pending its official launch — visit tempoapp.app to subscribe for launch notification"
-
-  version "0.2.6,12"
-  sha256 "6a5a7a1a1c6be35a24da618a0c96502dd29897c0154c73f0f746561c45ea3588"
+  version "0.3.1,18"
+  sha256 "fe9f3ec1783912da9cafb8fd53dc4db9c6876153396393ebd8f51a44845dd0e5"
 
   url "https://downloads.tempoapp.app/Tempo-#{version.csv.first}.dmg"
   name "Tempo"
